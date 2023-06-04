441b03027cd13d2f34391e1b7684a9fc2e5836c4
Robin <robin.noormets@voco.ee>
2023-06-04 21:05:18 +0300

    Use dynamic style class in form input validation

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -24,7 +24,7 @@ const CourseInput = props => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className="form-control">
+            <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
                 <label>Course Goal</label>
                 <input type="text" onChange={goalInputChangeHandler} />
                 <label style={{color:!isValid ? 'red' : 'black'}}>Course Goal</label>
